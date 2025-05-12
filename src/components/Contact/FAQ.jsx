f066110b933c8ae284b8705c1f3116764886e0e6
teymee <teymee17@gmail.com>
2025-05-12 15:08:38 +0100

    updates

--- src/components/Contact/FAQ.jsx
+++ src/components/Contact/FAQ.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import FAQItem from "./FAQItem";
-
-
+import { useTranslator } from "@/hooks/useTranslator";
 
 export default function FAQ() {
+  const t = useTranslator();
 
-    const faqs= [
-        {
-            title:"What is Fuelmetrics?",
-            body:"Fuelmetrics Limited is a technology company that provides innovative and affordable technology solutions to help businesses thrive"
-        },
-        {
-            title:"How can i install  epump in my stations?",
-            body:"Fuelmetrics Limited is a technology company that provides innovative and affordable technology solutions to help businesses thrive"
-        },
-        {
-            title:"Where is Fuelmetrics?",
-            body:"Fuelmetrics Limited is a technology company that provides innovative and affordable technology solutions to help businesses thrive"
-        },
-        {
-            title:"How can i install  epump in my station?",
-            body:"Fuelmetrics Limited is a technology company that provides innovative and affordable technology solutions to help businesses thrive"
-        },
-    ]
+  const faqs = [
+    {
+      title: t("contact.whatIsFuelmetrics"),
+      body: t("contact.ans1"),
+    },
+    {
+      title: "How can i install  epump in my stations?",
+      body: "Fuelmetrics Limited is a technology company that provides innovative and affordable technology solutions to help businesses thrive",
+    },
+    {
+      title: "Where is Fuelmetrics?",
+      body: "Fuelmetrics Limited is a technology company that provides innovative and affordable technology solutions to help businesses thrive",
+    },
+    {
+      title: "How can i install  epump in my station?",
+      body: "Fuelmetrics Limited is a technology company that provides innovative and affordable technology solutions to help businesses thrive",
+    },
+  ];
   return (
     <section className="bg-[#C8CCD330]">
       <section className="container [ lg:py-20 pt-[310px] pb-20 ]  ">
@@ -31,10 +31,9 @@ export default function FAQ() {
         </div>
 
         <section className="space-y-4 [ lg:mt-10 mt-5 ] ">
-            {faqs.map((faq, index) => (
-                <FAQItem faq={faq} key={index} />
-            ))}
-
+          {faqs.map((faq, index) => (
+            <FAQItem faq={faq} key={index} />
+          ))}
         </section>
       </section>
     </section>
--- src/components/Contact/Hero.jsx
+++ src/components/Contact/Hero.jsx
@@ -2,8 +2,10 @@
 import email from "@/assets/svg/white-email.svg";
 import phone from "@/assets/svg/white-phone.svg";
 import address from "@/assets/svg/white-address.svg";
+import { useTranslator } from "@/hooks/useTranslator";
 
 export default function Hero() {
+  const t = useTranslator()
   return (
     <section className="hero-bg h-screen 3xl:h-[60vh]">
       <section className="  mx-auto flex-start [ flex-col lg:flex-row gap-y-4 ]  [ lg:w-9/12 w-[90%] ]  [ lg:pt-[10%] pt-[30%] ]">
@@ -15,16 +17,16 @@ export default function Hero() {
               <img src={address} alt="" />
 
               <section className="text-left space-y-6">
-                <p className="font-bold text-[20px]">Address</p>
+                <p className="font-bold text-[20px]">{t('footer.address')}</p>
                 <div className="space-y-2">
-                  <h2 className="text-[18px] font-medium">Head office</h2>
+                  <h2 className="text-[18px] font-medium">{t('contact.headOffice')}</h2>
                   <p className="text-sm">
                     18 Ilupeju by-pass, <br /> Lagos
                   </p>
                 </div>
                 <section className="flex-items-center [ lg:gap-x-8 gap-x-6 ]">
                   <div className="space-y-2">
-                    <h2 className="text-[18px] font-medium">Abuja office</h2>
+                    <h2 className="text-[18px] font-medium">Abuja {t('contact.office')}</h2>
                     <p className="text-sm">
                       Plot 6, Asokoro district, <br />
                       Abuja
@@ -32,7 +34,7 @@ export default function Hero() {
                   </div>
                   <div className="w-[1px] h-[60px] bg-white"></div>
                   <div className="space-y-2">
-                    <h2 className="text-[18px] font-medium">PH office</h2>
+                    <h2 className="text-[18px] font-medium">PH {t('contact.office')}</h2>
                     <p className="text-sm">
                       42, Trans-amadi road, <br />
                       Port, Harcourt
@@ -47,7 +49,7 @@ export default function Hero() {
               <img src={phone} alt="" className="self-start" />
 
               <section className="text-left space-y-2 font-bold">
-                <p className="text-[20px]">Telephone</p>
+                <p className="text-[20px]">{t('contact.telephone')}</p>
                 <p className="text-sm text-green-600">+234 913 104 8999</p>
               </section>
             </section>
@@ -57,7 +59,7 @@ export default function Hero() {
               <img src={email} alt="" className="self-start" />
 
               <section className="text-left space-y-2 font-bold">
-                <p className="text-[20px]">Email</p>
+                <p className="text-[20px]">{t('contact.email')}</p>
                 <p className="text-sm text-green-600">enquiries@epump.africa</p>
                 <p className="text-sm text-green-600">info@epump.africa</p>
               </section>
@@ -69,7 +71,7 @@ export default function Hero() {
           <section className="bg-white [ lg:h-[700px] lg:pt-40 lg:py-20 py-10 ] rounded-4xl ">
             <section className="w-[75%] mx-auto space-y-4">
               <h2 className="text-[20px] text-left font-bold">
-                Send us a message
+                {t('contact.sendMsg')}
               </h2>
               <section className="space-y-8">
                 <input
@@ -94,7 +96,7 @@ export default function Hero() {
 
                 <div className="w-full  center-content">
                   <button className="bg-primary text-white w-9/12 py-2 rounded-lg">
-                    Send
+                  {t('contact.send')}
                   </button>
                 </div>
               </section>
